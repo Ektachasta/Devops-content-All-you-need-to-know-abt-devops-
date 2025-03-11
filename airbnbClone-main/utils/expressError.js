4e10d8a2149be7fefa4b3339561e6c72656396f8
Ekta <ektachasta15@gmail.com>
2025-03-11 12:53:06 +0530

    First upload of my DevOps content

--- airbnbClone-main/utils/expressError.js
+++
@@ -1,8 +0,0 @@
-class ExpressError extends Error{
-    constructor(status,message){
-        super(message);
-        this.status = Number(status) || 500;
-    }
-}
-
-module.exports = ExpressError;
--- airbnbClone-main/utils/wrapAsync.js
+++
@@ -1,7 +0,0 @@
-const wrapAsync = (fn) => {
-    return function (req, res, next) {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    };
-};
-
-module.exports = wrapAsync;
